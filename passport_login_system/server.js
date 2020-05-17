7da28a13995246b0a3b5d6e30e609d16d5c7c69a
vio105 <vio105@mail.usask.com>
2020-05-16 19:19:02 -0600

    created passport-config.js, setup a function to handle multiple user authentication

--- passport_login_system/server.js
+++ passport_login_system/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const bcrpyt = require('bcrypt')
+const passport =require('passport')
+const initializePassport = require('./passport-config')
+initializePassport(passport)
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'victor'})
@@ -11,12 +18,29 @@ app.get('/login',(req,res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) =>{
+    
+})
+
 app.get('/register',(req,res) => {
     res.render('register.ejs')
 })
 
 app.post('/register', (req, res) =>{
-    
+    try{
+        const hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashPassword
+        })
+        res.redirect('/login')
+    } catch{
+        res.redirect('/register')
+    }
+    console.log(users)
+
 })
 
 app.listen(3000)
